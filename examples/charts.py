23d894cdf35773cd38186db754e280972e4a30f4
Daylin Morgan <daylinmorgan@gmail.com>
2022-10-06 12:39:39 -0500

    chore: add more examples

--- examples/charts.py
+++ examples/charts.py
@@ -59,8 +59,55 @@ def plot_area():
     )
 
 
-def plot_paths():
-    source = pd.DataFrame(
+def plot_wheat_wages():
+    base_wheat = alt.Chart(data.wheat.url).transform_calculate(
+        year_end="+datum.year + 5"
+    )
+
+    base_monarchs = alt.Chart(data.monarchs.url).transform_calculate(
+        offset="((!datum.commonwealth && datum.index % 2) ? -1: 1) * 2 + 95",
+        off2="((!datum.commonwealth && datum.index % 2) ? -1: 1) + 95",
+        y="95",
+        x="+datum.start + (+datum.end - +datum.start)/2",
+    )
+
+    bars = base_wheat.mark_bar(**{"fill": "#aaa", "stroke": "#999"}).encode(
+        x=alt.X("year:Q", axis=alt.Axis(format="d", tickCount=5)),
+        y=alt.Y("wheat:Q", axis=alt.Axis(zindex=1)),
+        x2=alt.X2("year_end"),
+    )
+
+    area = base_wheat.mark_area(**{"color": "#a4cedb", "opacity": 0.7}).encode(
+        x=alt.X("year:Q"), y=alt.Y("wages:Q")
+    )
+
+    area_line_1 = area.mark_line(**{"color": "#000", "opacity": 0.7})
+    area_line_2 = area.mark_line(**{"yOffset": -2, "color": "#EE8182"})
+
+    top_bars = base_monarchs.mark_bar(stroke="#000").encode(
+        x=alt.X("start:Q"),
+        x2=alt.X2("end"),
+        y=alt.Y("y:Q"),
+        y2=alt.Y2("offset"),
+        fill=alt.Fill(
+            "commonwealth:N", legend=None, scale=alt.Scale(range=["black", "white"])
+        ),
+    )
+
+    top_text = base_monarchs.mark_text(
+        **{"yOffset": 14, "fontSize": 9, "fontStyle": "italic"}
+    ).encode(x=alt.X("x:Q"), y=alt.Y("off2:Q"), text=alt.Text("name:N"))
+
+    return (
+        (bars + area + area_line_1 + area_line_2 + top_bars + top_text)
+        .properties(width=900, height=400)
+        .configure_axis(title=None, gridColor="white", gridOpacity=0.25, domain=False)
+        .configure_view(stroke="transparent")
+    )
+
+
+def make_animal_df():
+    return pd.DataFrame(
         [
             {"country": "Great Britain", "animal": "cattle"},
             {"country": "Great Britain", "animal": "cattle"},
@@ -101,6 +148,30 @@ def plot_paths():
             {"country": "United States", "animal": "sheep"},
         ]
     )
+
+
+def plot_emojis():
+    source = make_animal_df()
+
+    return (
+        alt.Chart(source)
+        .mark_text(size=45, baseline="middle")
+        .encode(
+            alt.X("x:O", axis=None),
+            alt.Y("animal:O", axis=None),
+            alt.Row("country:N", header=alt.Header(title="")),
+            alt.Text("emoji:N"),
+        )
+        .transform_calculate(
+            emoji="{'cattle': '🐄', 'pigs': '🐖', 'sheep': '🐏'}[datum.animal]"
+        )
+        .transform_window(x="rank()", groupby=["country", "animal"])
+        .properties(width=550, height=140)
+    )
+
+
+def plot_paths():
+    source = make_animal_df()
 
     domains = ["person", "cattle", "pigs", "sheep"]
 
--- examples/make-examples.py
+++ examples/make-examples.py
@@ -1,5 +1,12 @@
 from altair_saver import save
-from charts import plot_area, plot_bar, plot_map, plot_paths
+from charts import (
+    plot_area,
+    plot_bar,
+    plot_emojis,
+    plot_map,
+    plot_paths,
+    plot_wheat_wages,
+)
 
 from okab.saver import OkabSaver
 
@@ -12,11 +19,14 @@ def main():
 
     plot_bar().save("example-bar.svg", method=OkabSaver)
     plot_bar().save("example-bar.png", method=OkabSaver)
+    plot_bar().properties(width=1000,height=1000).save("example-bar-large.png", method=OkabSaver)
     plot_bar().save("example-bar-scaled.png", method=OkabSaver, scale_factor=5.0)
 
     plot_map().save("example-map.png", method=OkabSaver)
     plot_area().save("example-area.png", method=OkabSaver)
     plot_paths().save("example-paths.png", method=OkabSaver)
+    plot_emojis().save("example-emojis.png", method=OkabSaver)
+    plot_wheat_wages().save("example-wheat-wages.png", method=OkabSaver)
 
 
 if __name__ == "__main__":
